c1c5456f1fae2afb17ca9de9140a22b1efa8771c
sbgood <sam@sbgood.ca>
2020-12-21 12:09:52 -0500

    add eslint, loading test

--- shabegom/main.js
+++ shabegom/main.js
@@ -1,5 +1,9 @@
 (() => {
   roam42.shabegom = {};
 
-  roam42.shabegom.helloWorld = () => "hello world";
+  roam42.shabegom.helloWorld = () => 'hello world';
+  roam42.loader.addScriptToPage(
+    'randomWord',
+    `${roam42.host}shabegom/randomWord.js`,
+  );
 })();
