b79900ee32b3898bcf0c52a7d9c99d49c8680f5b
sbgood <sam@sbgood.ca>
2020-12-22 18:54:18 -0500

    moved all fetching to server

--- shabegom/dataFetching.js
+++ shabegom/dataFetching.js
@@ -44,34 +44,9 @@
 
   roam42.shabegom.pinboard = async (num = 1) =>
     await fetch(
-      "https://shbgm-cors.glitch.me/https://api.pinboard.in/v1/posts/recent?auth_token=sbgood:ee4aaf2efcd6c712c5f4&count=1&format=json&toread=yes"
-    )
-      .then(res => res.json())
-      .then(async data => {
-        const readLink = `https://cors-anywhere.herokuapp.com/https://api.pinboard.in/v1/posts/add?url=${data.posts[0].href}&description=${data.posts[0].description}&auth_token=sbgood:ee4aaf2efcd6c712c5f4`;
-        await fetch(readLink)
-          .then(res => {
-            roam42.help.displayMessage("Marked Read", 2000);
-          })
-          .catch(e => {
-            roam42.help.displayMessage(`Error!${e}`, 2000);
-          });
-        const page = `[[${data.posts[0].description}]]`;
-        const link = `[${data.posts[0].description}](${data.posts[0].href}) #readlater`;
-        const rawPage = data.posts[0].description;
-        return { page, link, rawPage };
-      });
+      `https://shabegom-roam-helpers.glitch.me/pinboard?num=${num}`
+    ).then(res => res.json());
 
-  roam42.shabegom.pbMarkRead = async () => {
-    const link = `https://shbgm-cors.glitch.me/https://api.pinboard.in/v1/posts/add?url=${shabegom.pb.url}&description=${shabegom.pb.title}&auth_token=sbgood:ee4aaf2efcd6c712c5f4`;
-    await fetch(link)
-      .then(res => {
-        roam42.help.displayMessage("Marked Read", 2000);
-      })
-      .catch(e => {
-        roam42.help.displayMessage(`Error!${e}`, 2000);
-      });
-  };
   roam42.shabegom.getMoon = async () => {
     const url = await fetch(
       "https://shabegom-roam-helpers.glitch.me/moon"
