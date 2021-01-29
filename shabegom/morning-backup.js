93a4b39d4996f67e66de8a57e3c6b31c28af867f
sbgood <sam@sbgood.ca>
2021-01-29 11:04:30 -0500

    refactor morning

--- shabegom/morning-backup.js
+++ shabegom/morning-backup.js
@@ -3,82 +3,61 @@
   roam42.shabegom.morning = async () => {
     // Data Fetching
     await roam42.help.displayMessage("Fetching Data", 2000);
-
-    // Readwise Output
-    try {
     const readwise = await roam42.shabegom.fetchReadwise();
     await roam42.help.displayMessage("Readwise Fetched", 2000);
-    await roam42.shabegom.multiSection("#Readwise", readwise);
-    } catch() {
-        console.log('readwise failed')
-    }
-
-    // Moon
-    try {
-    const moon = await roam42.shabegom.getMoon();
-    await roam42.shabegom.multiSection("Moon 🌝", [moon.phase, moon.url]);
-    }
-    catch () {
-        console.log('moon failed')
-    }
-
-    // Horoscope
-    try {
     const horoscope = await roam42.shabegom.horoscope();
     await roam42.help.displayMessage("Horoscope Fetched", 2000);
     const pattern = await roam42.shabegom.pattern();
     await roam42.help.displayMessage("Pattern Fetched", 2000);
+    const shakespeare = await roam42.shabegom.shakespeare();
+    await roam42.help.displayMessage("Shakespeare Fetched", 2000);
+    const stoic = await roam42.shabegom.stoic();
+    await roam42.help.displayMessage("Stoic Fetched", 2000);
+    const religion = await roam42.shabegom.religion();
+    await roam42.help.displayMessage("Religion Fetched", 2000);
+    const wiki = await roam42.shabegom.randomWiki();
+    await roam42.help.displayMessage("Wiki Fetched", 2000);
+    const poem = await roam42.shabegom.poem();
+    await roam42.help.displayMessage("Poem Fetched", 2000);
+    const buddha = await roam42.shabegom.quote("author", "gautama+buddha");
+    await roam42.help.displayMessage("Buddha Fetched", 2000);
+    const quote = await roam42.shabegom.quote("title", roam42.shabegom.word());
+    await roam42.help.displayMessage("Quote Fetched", 2000);
+    const pinboard = await roam42.shabegom.pinboard(1);
+    await roam42.help.displayMessage("Pinboard Fetched", 2000);
+    const moon = await roam42.shabegom.getMoon();
+    const untetheredQuote = await roam42.common.getRandomBlockFromPage(
+      "The Untethered Soul"
+    );
+    await roam42.help.displayMessage("Data Fetch Complete", 2000);
+
+    // Readwise Output
+    await roam42.shabegom.multiSection("#Readwise", readwise);
+
+    // Moon
+    await roam42.shabegom.multiSection("Moon 🌝", [moon.phase, moon.url]);
+
+    // Horoscope
     await roam42.shabegom.multiSection("#Horoscope 🐐", [
       `> ${horoscope.summary}`,
       `**Do:** ${horoscope.todo.join(", ")}`,
       `> ${pattern.summary}`
     ]);
-    } catch() {
-        console.log('horoscope failed')
-    }
     // Shakespeare
-    try {
-    const shakespeare = await roam42.shabegom.shakespeare();
-    await roam42.help.displayMessage("Shakespeare Fetched", 2000);
     await roam42.shabegom.section("#Shakespeare", shakespeare);
-    } catch() {
-        console.log('shakespeare failed')
-    }
 
     // Quotes Start
-    try {
-    const untetheredQuote = await roam42.common.getRandomBlockFromPage(
-      "The Untethered Soul"
-    );
-    const stoic = await roam42.shabegom.stoic();
-    await roam42.help.displayMessage("Stoic Fetched", 2000);
-    const buddha = await roam42.shabegom.quote("author", "gautama+buddha");
-    await roam42.help.displayMessage("Buddha Fetched", 2000);
-    const quote = await roam42.shabegom.quote("title", roam42.shabegom.word());
-    await roam42.help.displayMessage("Quote Fetched", 2000);
     await roam42.shabegom.multiSection("#Quotes", [
       `> ((${untetheredQuote}))\n - [[Michael Singer]]`,
       stoic,
       `> ${buddha.quote} \n ${buddha.source} `,
       `> ${quote.quote} \n ${quote.source} `
     ]);
-    } catch() {
-        console.log('quotes failed')
-    }
 
     // Religion
-    try {
-    const religion = await roam42.shabegom.religion();
-    await roam42.help.displayMessage("Religion Fetched", 2000);
     await roam42.shabegom.bibleVerses(religion);
-    } catch() {
-        console.log('religion failed')
-    }
 
     // Wiki
-    try {
-    const wiki = await roam42.shabegom.randomWiki();
-    await roam42.help.displayMessage("Wiki Fetched", 2000);
     if (wiki.thumbnail) {
       await roam42.shabegom.multiSection(wiki.title, [
         wiki.thumbnail,
@@ -88,18 +67,9 @@
     } else {
       await roam42.shabegom.multiSection(wiki.title, [wiki.excerpt, wiki.url]);
     }
-    } catch() {
-        console.log('wiki failed')
-    }
 
     // Poem
-    try {
-    const poem = await roam42.shabegom.poem();
-    await roam42.help.displayMessage("Poem Fetched", 2000);
     await roam42.shabegom.section(poem.title, poem.poemContent);
-    } catch() {
-        console.log('poem failed')
-    }
 
     // Read Later Link
     const ref = await roam42.smartBlocks.getRandomBlocksMention("readlater");
@@ -110,9 +80,6 @@
     ]);
 
     // Pinboard
-    try {
-    const pinboard = await roam42.shabegom.pinboard(1);
-    await roam42.help.displayMessage("Pinboard Fetched", 2000);
     await roam42.shabegom.header("#pinboard");
     await roam42.smartBlocks.activeWorkflow.outputAdditionalBlock(
       pinboard[0].page
@@ -141,9 +108,5 @@
     } else {
       await history.go(-1);
     }
-    } catch() {
-        console.log('pinboard failed')
-    }
-    await roam42.help.displayMessage("Data Fetch Complete", 2000);
   };
 })();
